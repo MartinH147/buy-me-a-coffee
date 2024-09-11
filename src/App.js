a9259e4dcae7e4005bc2e6396f23762719da8826
MartinH147 <martinh13@optusnet.com.au>
2024-09-11 21:05:50 +1000

    made a start on the UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import coffeeCup from './coffee-cup.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='leftside'>
+        <img src={coffeeCup}/>
+      </div>
+      <div className='rightside'>
+        <h1>Buy Me A Coffee</h1>
+      </div>
     </div>
   );
 }
