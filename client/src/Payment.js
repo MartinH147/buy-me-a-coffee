ca6d5ec565af54f944ab705018921e78180476bf
MartinH147 <martinh13@optusnet.com.au>
2024-09-17 19:33:16 +1000

    started following stripe video

--- client/src/Payment.js
+++ client/src/Payment.js
@@ -1,10 +1,22 @@
+// https://www.youtube.com/watch?v=e-whXipfRvg @5.07
+
 import { useEffect, useState } from "react";
-import './App.css';
+import { loadStripe } from "@stripe/stripe-js"
 
 function Payment(props) {
+  const [stripePromise, setStripePromise] = useState(null);
+
+  useEffect(() => {
+    fetch("/config").then(async (r) => {
+      const { publishableKey } = await r.json();
+
+      setStripePromise(loadStripe(publishableKey))
+    })
+  }, [])
+
   return (
    <>
-   
+    <h1>React Stripe and the Payment ELement</h1>
    </>
   );
 }
