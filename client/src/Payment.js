1ac2dabff75d4b162287c2e4ae2d34ad40ba03df
MartinH147 <martinh13@optusnet.com.au>
2024-09-18 20:21:29 +1000

    errors in getting client secret from back end

--- client/src/Payment.js
+++ client/src/Payment.js
@@ -1,16 +1,28 @@
-// https://www.youtube.com/watch?v=e-whXipfRvg @5.07
+// https://www.youtube.com/watch?v=e-whXipfRvg @6:51 look back for ERRORS
 
 import { useEffect, useState } from "react";
 import { loadStripe } from "@stripe/stripe-js"
 
 function Payment(props) {
   const [stripePromise, setStripePromise] = useState(null);
+  const [clientSecret, setClientSecret] = useState("");
 
   useEffect(() => {
     fetch("/config").then(async (r) => {
       const { publishableKey } = await r.json();
 
       setStripePromise(loadStripe(publishableKey))
+    })
+  }, [])
+
+  useEffect(() => {
+    fetch("/create-user-intent", {
+      method: "POST",
+      body: JSON.stringify({})
+    }).then(async (r) => {
+      const { clientSecret } = await r.json();
+
+      console.log(clientSecret)
     })
   }, [])
 
