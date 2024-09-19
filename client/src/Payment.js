7d411dcb5f8e04fc02ed8e1f1fb7fd26e9e6dd2f
MartinH147 <martinh13@optusnet.com.au>
2024-09-19 16:06:59 +1000

    fixed bug, added some elements

--- client/src/Payment.js
+++ client/src/Payment.js
@@ -1,7 +1,9 @@
-// https://www.youtube.com/watch?v=e-whXipfRvg @6:51 look back for ERRORS
+// https://www.youtube.com/watch?v=e-whXipfRvg @8:36
 
 import { useEffect, useState } from "react";
 import { loadStripe } from "@stripe/stripe-js"
+import CheckoutForm from './CheckoutForm';
+import { Elements } from "@stripe/react-stripe-js"
 
 function Payment(props) {
   const [stripePromise, setStripePromise] = useState(null);
@@ -16,19 +18,24 @@ function Payment(props) {
   }, [])
 
   useEffect(() => {
-    fetch("/create-user-intent", {
+    fetch("/create-payment-intent", {
       method: "POST",
       body: JSON.stringify({})
     }).then(async (r) => {
       const { clientSecret } = await r.json();
 
-      console.log(clientSecret)
+      setClientSecret(clientSecret);
     })
   }, [])
 
   return (
    <>
     <h1>React Stripe and the Payment ELement</h1>
+    {stripePromise && clientSecret && (
+      <Elements stripe={stripePromise} options={{clientSecret}}>
+        <CheckoutForm />
+      </Elements>
+    )}
    </>
   );
 }
