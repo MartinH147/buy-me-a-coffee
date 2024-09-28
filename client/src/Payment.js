fa0a89e8c291f70cf5d09279880ac025d2a0cc14
MartinH147 <martinh13@optusnet.com.au>
2024-09-28 17:23:00 +1000

    added options for different payment amounts, going to connect to back end

--- client/src/Payment.js
+++ client/src/Payment.js
@@ -1,4 +1,4 @@
-// https://www.youtube.com/watch?v=e-whXipfRvg @11:51
+// https://www.youtube.com/watch?v=e-whXipfRvg
 
 import { useEffect, useState } from "react";
 import { loadStripe } from "@stripe/stripe-js"
@@ -36,7 +36,20 @@ function Payment(props) {
       <Spline scene="https://prod.spline.design/oAud9-tKqGkuyJP6/scene.splinecode" className="coffeeCup"/>
     </div>
     <div className="paymentContainer">
-      <h1>Buy Me a Coffee</h1>
+      <p>Thanks for stopping by! I appreciate your support (so do my taste buds).</p>
+      <div className="paymentOption">
+        <h2>Shout me a coffee</h2>
+        <button>$5</button>
+      </div>
+      <div className="paymentOption">
+        <h2>Or a kebab</h2>
+        <button>$15</button>
+      </div>
+      <div className="paymentOption">
+        <h2>Or a multi-course Michelin-starred meal</h2>
+        <button>$400</button>
+      </div>
+
       {stripePromise && clientSecret && (
         <Elements stripe={stripePromise} options={{clientSecret}} className="paymentBox">
           <CheckoutForm />
